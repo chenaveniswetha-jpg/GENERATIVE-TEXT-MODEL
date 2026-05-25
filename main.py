300e5912ef90de4fa932b54efc983708c210baa3
chenaveniswetha-jpg <chenaveniswetha@gmail.com>
2026-05-25 11:42:49 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from transformers import pipeline
 
+# load AI text generator
 generator = pipeline(
     "text-generation",
     model="gpt2"
 )
+
+# user input
 text = input("Enter your topic: ")
 
+# generate text
 result = generator(
     text,
     max_length=100,
     num_return_sequences=1
 )
+
+# display output
 print("\nGenerated Text:\n")
 
 print(result[0]['generated_text'])
